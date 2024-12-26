bbaedfddc61d3a837cd5773325fd32518773bfb6
p4prabhu <p4prabhat01@gmail.com>
2024-12-26 17:26:53 -0500

    added hello Dag scripts

---
+++ dags/hello.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'datamasteryLab.com',
+    'start_date': datetime(year:2024, month:1, day:25),
+    'catchup': False
+}
+
+dag = DAG(
+    'hello',
+    default_args=default_args,
+    schedule=timedelta(days=1)
+)
+t1 = BashOperator(
+    task_id = 'hello_world',
+    bash_command='echo "Hello World"',
+    dag=dag
+)
+t2 = BashOperator(
+    task_id = 'hello_dml',
+    bash_command='echo "Hello Data Mastery Lab"',
+    dag=dag
+)
+
+t1>>t2
